ae5ec2bc01fd8bd4cea4d72a5bc2e754a540dc85
Mel Drews <drewsm@onid.oregonstate.edu>
2015-04-21 08:11:54 -0400

    Updated src/variables.js Need to push to github to be able to clone to other hosts and work from other locations.

--- src/variables.js
+++ src/variables.js
@@ -14,7 +14,7 @@ function variableModification(a) {
   var asString;
   var yourNumberIs;
   //your code here
-
+    plus5 = a + 5;
   //end your code
   return [plus5, asString, yourNumberIs, a];
 }
